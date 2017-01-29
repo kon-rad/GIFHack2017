aeb831429ca433a9bef285f7dc1d3333b9004a85
Fernando1Borges <centraloro@gmail.com>
2017-01-29 07:46:58 -0800

    Add new ui

--- public/functions.js
+++ public/functions.js
@@ -3,6 +3,7 @@
 console.log("running");
 var q
 var n = 4;
+var idCount = 0;
 
 var myVideo=document.getElementById("thevideo");
 
@@ -121,10 +122,34 @@ function test(text){
 }
 var checkingStatus = function(n){
 
-	checking = window.setInterval(function(){
+	var checking = window.setInterval(function(){
 		console.log("yo");
-		getGfy(n);
+		if(true) {
+			//getGfy(n) != null
+			//q = getGfy(n);
+		//	var gifmp4Url = q.gifyItem.mp4Url
+      //console.log(JSON.stringify(getGfy(n)));
+    // console.log(gifmp4Url);
+     //gifyItem.mp4Url
+//{"gfyItem":{"gfyId":"dimuntriedgilamonster","gfyName":"DimUntriedGilamonster","gfyNumber":"716881701","webmUrl":"https://zippy.gfycat.com/DimUntriedGilamonster.webm","gifUrl":"https://giant.gfycat.com/DimUntriedGilamonster.gif","mobileUrl":"https://thumbs.gfycat.com/DimUntriedGilamonster-mobile.mp4","mobilePosterUrl":"https://thumbs.gfycat.com/DimUntriedGilamonster-mobile.jpg","miniUrl":"https://thumbs.gfycat.com/DimUntriedGilamonster-mini.mp4","miniPosterUrl":"https://thumbs.gfycat.com/DimUntriedGilamonster-mini.jpg","posterUrl":"https://thumbs.gfycat.com/DimUntriedGilamonster-poster.jpg","thumb360Url":"https://thumbs.gfycat.com/DimUntriedGilamonster-360.mp4","thumb360PosterUrl":"https://thumbs.gfycat.com/DimUntriedGilamonster-thumb360.jpg","thumb100PosterUrl":"https://thumbs.gfycat.com/DimUntriedGilamonster-thumb100.jpg","max5mbGif":"https://thumbs.gfycat.com/DimUntriedGilamonster-size_restricted.gif","max2mbGif":"https://thumbs.gfycat.com/DimUntriedGilamonster-small.gif","max1mbGif":"https://thumbs.gfycat.com/DimUntriedGilamonster-max-1mb.gif","gif100px":"https://thumbs.gfycat.com/DimUntriedGilamonster-100px.gif","mjpgUrl":"https://thumbs.gfycat.com/DimUntriedGilamonster.mjpg","width":1920,"height":1080,"avgColor":"#000000","frameRate":23,"numFrames":97,"mp4Size":2599459,"webmSize":795189,"gifSize":7173845,"source":8,"createDate":1485702230,"nsfw":5,"mp4Url":"https://fat.gfycat.com/DimUntriedGilamonster.mp4","likes":0,"published":1,"dislikes":0,"extraLemmas":"","md5":"3c00157f66212e72d2d8635fa2b4affd","views":1,"tags":null,"userName":"anonymous","title":"testupload","description":"","languageCategories":null,"url":"https://www.youtube.com/watch?v=DekuSxJgpbY","domainWhitelist":[]}}
+
+
+			var img = $('<video id="gifsID + ' + idCount + '" loop="loop" autoplay="autoplay"/>'); //Equivalent: $(document.createElement('img'))
+      img.attr('src', gifmp4Url);
+      img.appendTo('#imageDiv');
+			idCount++;
+			//clearInterval(checking);
+
+
+
+
+
+
+		} else {
+			console.log("Processing...");
+		}
 	}, 3000);
+	clearInterval(checking);
 
 }
 
@@ -132,7 +157,7 @@ var checkingStatus = function(n){
 function getGfy(n){
 	var xhttp = new XMLHttpRequest();
 	//xhttp.open("GET", url, true);
-	var endpoint = "https://api.gfycat.com/v1/gfycats/" + n;
+	var endpoint = "https://api.gfycat.com/v1test/gfycats/" + n;
 	xhttp.open("GET",endpoint, true);
 	//xhttp.setRequestHeader("name", n);
 
@@ -143,9 +168,12 @@ function getGfy(n){
 			console.log(xhttp.status);
 			//console.log(xhttp.responseText);
 			q = JSON.parse(xhttp.responseText);
-			console.log(q);
+			console.log(JSON.stringify(q));
+			return JSON.stringify(q);
 
-		};
+		} else {
+			return false;
+		}
 	};
 };
 
