e1c885948e3bb9697a98bcc62971d67093c54a3a
konradgnat <konradmgnat@gmail.com>
2018-12-29 19:05:59 -0600

    refactor

--- app.js
+++ app.js
@@ -1,184 +1,208 @@
-const Gfycat = require('gfycat-sdk');
+const Gfycat = require("gfycat-sdk");
 var gfycat = new Gfycat({
   clientId: "2_7kmTI8",
-  clientSecret: "9X4E1UlJMRmVUdCrqzeeZNkNCeud2oWIGHdX_PzwP2EgM7CiXrpMSgQ_kBuOd-Yc"
+  clientSecret:
+    "9X4E1UlJMRmVUdCrqzeeZNkNCeud2oWIGHdX_PzwP2EgM7CiXrpMSgQ_kBuOd-Yc"
 });
-var express = require('express');
-var http = require('http');
-const https = require('https');
-const assert = require('assert');
+var express = require("express");
+var http = require("http");
+const https = require("https");
+const assert = require("assert");
 
-var hostname = '0.0.0.0';
+var hostname = "0.0.0.0";
 var port = process.env.PORT || 8080;
 var app = express();
 var router = express.Router();
 
-app.use('/', router); //attach security router to everything that goes to /map
+app.use("/", router); //attach security router to everything that goes to /map
 
-app.use(express.static(__dirname + '/public')); //allow access to the .html files in public
-app.listen(port, hostname, function () {
+app.use(express.static(__dirname + "/public")); //allow access to the .html files in public
+app.listen(port, hostname, function() {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-
 gfycat.authenticate((err, data) => {
-  console.log('Your app is now authenticated', data);
+  console.log("Your app is now authenticated", data);
 });
 
 function errorHappened(err) {
-
   console.log("ERROR!");
   console.log(err);
 }
 
-router.route('/getdata')
-  .all(function (req, res, next) {
+router
+  .route("/getdata")
+  .all(function(req, res, next) {
     next();
-  }).get(function (req, res, next) {
-  var query = req.headers.query;
-  if (req.headers.count) {
-    gfycount = req.headers.count;
-  } else {
-    gfycount = 5;
-  }
-  options = {
-    search_text: query,
-    count: gfycount,
-    first: 0
-  };
+  })
+  .get(function(req, res, next) {
+    var query = req.headers.query;
+    if (req.headers.count) {
+      gfycount = req.headers.count;
+    } else {
+      gfycount = 5;
+    }
+    options = {
+      search_text: query,
+      count: gfycount,
+      first: 0
+    };
 
-  gfycat.search(options).then(data => {
-    console.log('gfycats', data);
-    res.json(data);
-  }).catch(err => {
-    errorHappened(err);
+    gfycat
+      .search(options)
+      .then(data => {
+        console.log("gfycats", data);
+        res.json(data);
+      })
+      .catch(err => {
+        errorHappened(err);
+      });
   });
 
-});
-
-router.route('/gfyinfo')
-  .all(function (req, res, next) {
+router
+  .route("/gfyinfo")
+  .all(function(req, res, next) {
     next();
-  }).get(function (req, res, next) {
-  var url = req.headers.url;
-  console.log("url " + url);
-  gfycat.getGifDetails({gfyId: url}).then(data => {
-    console.log('gfycats', data);
-    res.json(data);
-  }).catch(err => {
-    errorHappened(err);
+  })
+  .get(function(req, res, next) {
+    var url = req.headers.url;
+    console.log("url " + url);
+    gfycat
+      .getGifDetails({ gfyId: url })
+      .then(data => {
+        console.log("gfycats", data);
+        res.json(data);
+      })
+      .catch(err => {
+        errorHappened(err);
+      });
   });
-});
 
-router.route('/trending')
-  .all(function (req, res, next) {
+router
+  .route("/trending")
+  .all(function(req, res, next) {
     next();
-  }).get(function (req, res, next) {
-  if (req.headers.count) {
-    count = req.headers.count;
-  } else {
-    count = 1;
-  }
+  })
+  .get(function(req, res, next) {
+    if (req.headers.count) {
+      count = req.headers.count;
+    } else {
+      count = 1;
+    }
 
-  gfycat.trendingGifs({count: count}).then(data => {
-    console.log('trending response: ', data.gfycats[count - 1].gifUrl);
-    res.json(data.gfycats[count - 1]);
-  }).catch(err => {
-    errorHappened(err);
+    gfycat
+      .trendingGifs({ count: count })
+      .then(data => {
+        console.log("trending response: ", data.gfycats[count - 1].gifUrl);
+        res.json(data.gfycats[count - 1]);
+      })
+      .catch(err => {
+        errorHappened(err);
+      });
   });
-});
 
-router.route('/videourl')
-  .all(function (req, res, next) {
+router
+  .route("/videourl")
+  .all(function(req, res, next) {
     next();
-  }).get(function (req, res, next) {
-  var url = req.headers.url
-  console.log("url " + url)
+  })
+  .get(function(req, res, next) {
+    var url = req.headers.url;
+    console.log("url " + url);
 
-  options = {
-    host: 'api.gfycat.com',
-    path: '/v1/gfycats/fetch/remoteurlinfo',
-    headers: {url: url}
+    options = {
+      host: "api.gfycat.com",
+      path: "/v1/gfycats/fetch/remoteurlinfo",
+      headers: { url: url }
+    };
 
-  };
+    https.get(options, function(http_res) {
+      // initialize the container for our data
+      var data = "";
 
-  https.get(options, function (http_res) {
-    // initialize the container for our data
-    var data = "";
+      // this event fires many times, each time collecting another piece of the response
+      http_res.on("data", function(chunk) {
+        // append this chunk to our growing `data` var
+        data += chunk;
+        //console.log(data);
+      });
 
-    // this event fires many times, each time collecting another piece of the response
-    http_res.on("data", function (chunk) {
-      // append this chunk to our growing `data` var
-      data += chunk;
-      //console.log(data);
+      // this event fires *one* time, after all the `data` events/chunks have been gathered
+      http_res.on("end", function() {
+        // you can use res.send instead of console.log to output via express
+        console.log(data.url); //probably undefined unless I do a json parse
+        res.end(data); //If I do res.json, it doesn't work
+      });
     });
-
-    // this event fires *one* time, after all the `data` events/chunks have been gathered
-    http_res.on("end", function () {
-      // you can use res.send instead of console.log to output via express
-      console.log(data.url); //probably undefined unless I do a json parse
-      res.end(data); //If I do res.json, it doesn't work
-    });
   });
-});
 
-
-router.route('/uploadurl')
-  .all(function (req, res, next) {
+router
+  .route("/uploadurl")
+  .all(function(req, res, next) {
     next();
-  }).post(function (req, res, next) {
-  var url = req.headers.url;
-  var duration = req.headers.duration;
-  var start = req.headers.start;
-  console.log("url " + url);
-  console.log(req.headers);
+  })
+  .post(function(req, res, next) {
+    var url = req.headers.url;
+    var duration = req.headers.duration;
+    var start = req.headers.start;
+    console.log("url " + url);
+    console.log(req.headers);
 
-  //hardcoded for testing
-  options = {
-    fetchUrl: url,
-    title: "gifNote",
-    private: false,
-    cut: {duration: duration, start: start}
-  }
+    //hardcoded for testing
+    options = {
+      fetchUrl: url,
+      title: "gifNote",
+      private: false,
+      cut: { duration: duration, start: start }
+    };
 
-  gfycat.upload(options).then(data => {
-    console.log('uploaded response: ', data);
-    res.json(data);
-  }).catch(err => {
-    errorHappened(err);
+    gfycat
+      .upload(options)
+      .then(data => {
+        console.log("uploaded response: ", data);
+        res.json(data);
+      })
+      .catch(err => {
+        errorHappened(err);
+      });
   });
-});
 
-
-router.route('/checkstatus')
-  .all(function (req, res, next) {
+router
+  .route("/checkstatus")
+  .all(function(req, res, next) {
     next();
-  }).get(function (req, res, next) {
-  var id = req.headers.id
-  console.log("id " + id);
+  })
+  .get(function(req, res, next) {
+    var id = req.headers.id;
+    console.log("id " + id);
 
-
-  gfycat.checkUploadStatus(id).then(data => {
-    console.log('uploaded response: ', data);
-    res.json(data);
-  }).catch(err => {
-    errorHappened(err);
+    gfycat
+      .checkUploadStatus(id)
+      .then(data => {
+        console.log("uploaded response: ", data);
+        res.json(data);
+      })
+      .catch(err => {
+        errorHappened(err);
+      });
   });
-});
 
-router.route('/gfycats')
-  .all(function (name) {
+router
+  .route("/gfycats")
+  .all(function(name) {
     next();
+  })
+  .get(function(req, res, next) {
+    var name = req.headers.name;
+    console.log("name:" + name);
 
-  }).get(function (req, res, next) {
-  var name = req.headers.name
-  console.log("name:" + name);
-
-  gfycat.getGyfcat(name).then(data => {
-    console.log('get gfycat: ', data);
-    res.json(data);
-  }).catch(err => {
-    errorHappened(err);
+    gfycat
+      .getGyfcat(name)
+      .then(data => {
+        console.log("get gfycat: ", data);
+        res.json(data);
+      })
+      .catch(err => {
+        errorHappened(err);
+      });
   });
-});
-
--- public/js/index.js
+++ public/js/index.js
@@ -1,209 +1,236 @@
-
 var q;
 var n = 4;
 var idCount = 0;
-
 var curTime = 0;
 var myVideo = document.getElementById("thevideo");
-const startInput = $("#startInput");
-const gfyRange = $('#gfy-range');
-var duration = (parseInt(curTime) - parseInt(startInput.val() > 15)) ? 15 : parseInt(curTime) - parseInt(startInput.val());
+var duration =
+  parseInt(curTime) - parseInt($("#startInput").val() > 15)
+    ? 15
+    : parseInt(curTime) - parseInt($("#startInput").val());
 var isLooping = false;
-var start = startInput.val(curTime);
-
-
+var start = $("#startInput").val(curTime);
 var theLoop; //global
 var videoUrl; //the is the source of the raw video;"r---sn...."
 var yt_URL; //"youtube.com/..."
 
 $(document).ready(function() {
-	gfyRange.defaultValue = 15;
-	gfyRange.on('change', function(){
-		duration = parseInt(gfyRange.val());
-		if (!isLooping){
-			isLooping=true;
-			startLoop();
-
-		}
-			$("#stopInput").val(parseInt(startInput.val()) + parseInt(gfyRange.val()));
-	});
+  $("#gfy-range").defaultValue = 15;
+  $("#gfy-range").on("change", function() {
+    duration = parseInt($("#gfy-range").val());
+    if (!isLooping) {
+      isLooping = true;
+      startLoop();
+    }
+    $("#stopInput").val(
+      parseInt($("#startInput").val()) + parseInt($("#gfy-range").val())
+    );
+  });
 });
 
-function setVideoUrl(url){
-	videoUrl = url;
-	$("#thevideo").attr("src",url);
+function setVideoUrl(url) {
+  videoUrl = url;
+  $("#thevideo").attr("src", url);
 }
 
-function setStartTime(){
-
-		curTime = Math.floor(document.getElementById("thevideo").currentTime);
-		startInput.val(curTime);
-		start=(curTime)
+function setStartTime() {
+  curTime = Math.floor(document.getElementById("thevideo").currentTime);
+  $("#startInput").val(curTime);
+  start = curTime;
+  console.log(curTime, $("#startInput"), start);
 }
 
-function setEndTime(){
-	curTime = Math.floor(document.getElementById("thevideo").currentTime);
-	if (parseInt(curTime) - parseInt(startInput.val() > 15)){
-				$("#stopInput").val(parseInt(startInput.val() + 15));
-	} else {
-		$("#stopInput").val(curTime);
-	}
+function setEndTime() {
+  curTime = Math.floor(document.getElementById("thevideo").currentTime);
+  if (parseInt(curTime) - parseInt($("#startInput").val() > 15)) {
+    $("#stopInput").val(parseInt($("#startInput").val() + 15));
+  } else {
+    $("#stopInput").val(curTime);
+  }
 }
 
-function startLoop(){
-	theLoop = window.setInterval(function(){
-		if (document.getElementById("thevideo").currentTime>start+duration){
-			document.getElementById("thevideo").currentTime=start;
-		}
-	}, 100);
+function startLoop() {
+  theLoop = window.setInterval(function() {
+    if (document.getElementById("thevideo").currentTime > start + duration) {
+      document.getElementById("thevideo").currentTime = start;
+    }
+  }, 100);
 
-	return theLoop
+  return theLoop;
 }
 
-function stopLoop(){
-	clearInterval(theLoop)
+function stopLoop() {
+  clearInterval(theLoop);
 }
 
-function getTrending(){
-	var xhttp = new XMLHttpRequest();
-	var endpoint = "/trending";
-	xhttp.open("GET",endpoint);
-	xhttp.setRequestHeader("count",n);
-	n+=1;
-	xhttp.send();
-	xhttp.onreadystatechange = function() {
-		if (xhttp.readyState === 4 && xhttp.status === 200) {
-			q = JSON.parse(xhttp.responseText);
-			insertGif(q);
-		}
-	};
+/**
+ * Gets list of trending gfycat gifs
+ */
+function getTrending() {
+  var xhttp = new XMLHttpRequest();
+  var endpoint = "/trending";
+  xhttp.open("GET", endpoint);
+  xhttp.setRequestHeader("count", n);
+  n += 1;
+  xhttp.send();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+      q = JSON.parse(xhttp.responseText);
+      insertGif(q);
+    }
+  };
 }
 
+/**
+ * Checks status of gif upload via fetch to gfycat api
+ * @param gifName String
+ */
+function checkUploadStatus(gifName) {
+  const CHECK_STATUS_ENDPOINT =
+    "https://api.gfycat.com/v1/gfycats/fetch/status/";
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("GET", CHECK_STATUS_ENDPOINT + gifName);
+  xhttp.send();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+      console.log("check status ready: ", xhttp.readyState);
+      console.log(xhttp.status, JSON.parse(xhttp.responseText));
+    }
+  };
+}
+
+/**
+ * Initiates gif creation via Post request to Gfycat with video url, start and end times
+ */
 function createGfy() {
   const url = $("#video-url-input").val();
-  start = startInput.val();
-	duration = $("#stopInput").val() - startInput.val();
+  start = $("#startInput").val();
+  duration = $("#stopInput").val() - $("#startInput").val();
 
-	const xhttp = new XMLHttpRequest();
-	const endpoint = "/uploadurl";
-	xhttp.open("POST",endpoint);
-	xhttp.setRequestHeader("start",start);
-	xhttp.setRequestHeader("url",url);
-	xhttp.setRequestHeader("duration",duration);
-	xhttp.send();
-	xhttp.onreadystatechange = function() {
-		if (xhttp.readyState === 4 && xhttp.status === 200) {
-			console.log(xhttp.readyState);
-			console.log(xhttp.status);
-			q = JSON.parse(xhttp.responseText);
-			const gn = q.gfyname;
-			status=false;
+  const xhttp = new XMLHttpRequest();
+  const endpoint = "/uploadurl";
+  xhttp.open("POST", endpoint);
+  xhttp.setRequestHeader("start", start);
+  xhttp.setRequestHeader("url", url);
+  xhttp.setRequestHeader("duration", duration);
+  xhttp.send();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+      console.log(xhttp.readyState);
+      console.log(xhttp.status);
+      q = JSON.parse(xhttp.responseText);
+      const gifName = q.gfyname;
+      status = false;
 
       // this loop checks too see if gif is ready to fetch from gfycat
-			// TODO: Make function that uses the check gfycat endpoint
-			// TODO: Create indication for user that Gif is being created
-			uploadloop = setInterval(function(){
-				getGfy(gn);
-				console.log("checking");
-			}, 3000)
-    };
+      // TODO: Make function that uses the check gfycat endpoint
+      // TODO: Create indication for user that Gif is being created
+      uploadloop = setInterval(function() {
+        checkUploadStatus(gifName);
+        console.log("checking");
+      }, 3000);
+    }
   };
-};
+}
+
 var uploadloop;
 
-function getYT(url){
-	url = $("#video-url-input").val();
-	yt_URL = url;
-	var xhttp = new XMLHttpRequest();
-	var endpoint = "/videourl";
-	xhttp.open("GET",endpoint);
-	xhttp.setRequestHeader("url",url);
-	xhttp.send();
-	xhttp.onreadystatechange = function() {
-		if (xhttp.readyState === 4 && xhttp.status === 200) {
-			q = JSON.parse(xhttp.responseText);
-			var videoURL = q.url;
-			setVideoUrl(videoURL);
-		}
-	};
+function getYT(url) {
+  url = $("#video-url-input").val();
+  yt_URL = url;
+  var xhttp = new XMLHttpRequest();
+  var endpoint = "/videourl";
+  xhttp.open("GET", endpoint);
+  xhttp.setRequestHeader("url", url);
+  xhttp.send();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+      q = JSON.parse(xhttp.responseText);
+      var videoURL = q.url;
+      setVideoUrl(videoURL);
+    }
+  };
 }
 
 // adds gif to display
-function updateImage(gifUrl){
-	const img = $('<video style="width: 700px;" id="gifsID + ' + idCount + '" loop="loop" autoplay="autoplay"/>'); //Equivalent: $(document.createElement('img'))
-	img.attr('src', gifUrl);
+function updateImage(gifUrl) {
+  const img = $(
+    '<video style="width: 700px;" id="gifsID + ' +
+      idCount +
+      '" loop="loop" autoplay="autoplay"/>'
+  ); //Equivalent: $(document.createElement('img'))
+  img.attr("src", gifUrl);
 
-	const note = new NoteTemplate;
-	cards.push(note);
-	note.url = gifUrl;
-	reflow();
+  const note = new NoteTemplate();
+  cards.push(note);
+  note.url = gifUrl;
+  reflow();
 
-	idCount++;
-	clearInterval(uploadloop);
+  idCount++;
+  clearInterval(uploadloop);
 }
 
 // This function tries to download gif from gfycat when it is ready
-function getGfy(n){
-	const xhttp = new XMLHttpRequest();
-	const endpoint = "https://api.gfycat.com/v1test/gfycats/" + n;
-	xhttp.open("GET",endpoint, true);
+function getGfy(n) {
+  const xhttp = new XMLHttpRequest();
+  const endpoint = "https://api.gfycat.com/v1test/gfycats/" + n;
+  xhttp.open("GET", endpoint, true);
 
-	xhttp.send();
-	xhttp.onreadystatechange = function() {
-		if (xhttp.readyState === 4 && xhttp.status === 200) {
+  xhttp.send();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+      // gif is downloaded
+      q = JSON.parse(xhttp.responseText);
+      console.log(JSON.stringify(q));
+      status = true;
 
-		  // gif is downloaded
-			q = JSON.parse(xhttp.responseText);
-			console.log(JSON.stringify(q));
-			status=true;
-
-			// gif is displayed
-			updateImage(q.gfyItem.webmUrl);
-		}
-	};
+      // gif is displayed
+      updateImage(q.gfyItem.webmUrl);
+    }
+  };
 }
 
+var group1 = [];
+var group2 = [];
 
-var group1=[];
-var group2=[];
-
-function insertGif(gfy){
-	c1='<tr><img src="'+gfy.gifUrl+'" /></tr>'
-	document.getElementById("column1").innerHTML= c1;
+function insertGif(gfy) {
+  c1 = '<tr><img src="' + gfy.gifUrl + '" /></tr>';
+  document.getElementById("column1").innerHTML = c1;
 }
 
 // Draggable Table Functions
 var dragSrcEl = null;
 
-var cards = new Array;
-function NoteTemplate () {
-	this.index = null;
-	this.url = null;
-	this.noteText = null;
-	this.row = null;
-	this.column=null;
-};
+var cards = new Array();
 
-var cols = document.querySelectorAll('.draggable-element');
-function addListeners () {
-  cols = document.querySelectorAll('.draggable-element');
+function NoteTemplate() {
+  this.index = null;
+  this.url = null;
+  this.noteText = null;
+  this.row = null;
+  this.column = null;
+}
+
+var cols = document.querySelectorAll(".draggable-element");
+
+function addListeners() {
+  cols = document.querySelectorAll(".draggable-element");
   [].forEach.call(cols, function(col) {
-    col.addEventListener('dragstart', handleDragStart, false);
-    col.addEventListener('dragenter', handleDragEnter, false)
-    col.addEventListener('dragover', handleDragOver, false);
-    col.addEventListener('dragleave', handleDragLeave, false);
-    col.addEventListener('drop', handleDrop, false);
-    col.addEventListener('dragend', handleDragEnd, false);
+    col.addEventListener("dragstart", handleDragStart, false);
+    col.addEventListener("dragenter", handleDragEnter, false);
+    col.addEventListener("dragover", handleDragOver, false);
+    col.addEventListener("dragleave", handleDragLeave, false);
+    col.addEventListener("drop", handleDrop, false);
+    col.addEventListener("dragend", handleDragEnd, false);
   });
 }
 
 function handleDragStart(e) {
-// this / e.target is the source node.
-  this.classList.add('fadeOut');
+  // this / e.target is the source node.
+  this.classList.add("fadeOut");
 
   dragSrcEl = this;
-  e.dataTransfer.effectAllowed = 'move';
-  e.dataTransfer.setData('text/html', this.innerHTML);
+  e.dataTransfer.effectAllowed = "move";
+  e.dataTransfer.setData("text/html", this.innerHTML);
 }
 
 function handleDragOver(e) {
@@ -211,18 +238,18 @@ function handleDragOver(e) {
     e.preventDefault(); // Necessary. Allows us to drop.
   }
 
-  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+  e.dataTransfer.dropEffect = "move"; // See the section on the DataTransfer object.
 
   return false;
 }
 
 function handleDragEnter(e) {
   // this / e.target is the current hover target.
-  this.classList.add('over');
+  this.classList.add("over");
 }
 
 function handleDragLeave(e) {
-  this.classList.remove('over');  // this / e.target is previous target element.
+  this.classList.remove("over"); // this / e.target is previous target element.
 }
 
 function handleDrop(e) {
@@ -236,13 +263,13 @@ function handleDrop(e) {
   if (dragSrcEl !== this) {
     // Set the source column's HTML to the HTML of the column we dropped on.
 
-	var DraggedText = dragSrcEl.querySelector('#txtBox').value
-	var SmashedText = this.querySelector('#txtBox').value
-	dragSrcEl.innerHTML = this.innerHTML;
-    this.innerHTML = e.dataTransfer.getData('text/html');
+    var DraggedText = dragSrcEl.querySelector("#txtBox").value;
+    var SmashedText = this.querySelector("#txtBox").value;
+    dragSrcEl.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData("text/html");
 
-	this.querySelector('#txtBox').value = DraggedText;
-	dragSrcEl.querySelector('#txtBox').value = SmashedText;
+    this.querySelector("#txtBox").value = DraggedText;
+    dragSrcEl.querySelector("#txtBox").value = SmashedText;
   }
 
   return false;
@@ -250,45 +277,48 @@ function handleDrop(e) {
 
 function handleDragEnd(e) {
   // this/e.target is the source node.
-	this.classList.remove('fadeOut');
-  [].forEach.call(cols, function (col) {
-    col.classList.remove('over');
+  this.classList.remove("fadeOut");
+  [].forEach.call(cols, function(col) {
+    col.classList.remove("over");
   });
 }
 
-function reflow () {
-	var tableRef = document.getElementById('our_table');
-	tableRef.innerHTML = ""; //Erases the Table
+function reflow() {
+  var tableRef = document.getElementById("our_table");
+  tableRef.innerHTML = ""; //Erases the Table
 
-	for (i=0; i<cards.length; i++) {
-		var temp = cards[i];
-		var tmpl = document.getElementById('StepTemplate').content.cloneNode(true);
-		if (temp.url === "") {
-			tmpl.querySelector('#imgBox').outerHTML = "";
-			tmpl.querySelector('#txtBox').className="txtBox-textOnly";
-		} else {
-			tmpl.querySelector('#imgBox').innerHTML = '<video style="width: 100%;" loop="loop" autoplay="autoplay" src=" ' + temp.url + ' "/>'
-			tmpl.querySelector('#txtBox').className="txtBox";
-		}
+  for (i = 0; i < cards.length; i++) {
+    var temp = cards[i];
+    var tmpl = document.getElementById("StepTemplate").content.cloneNode(true);
+    if (temp.url === "") {
+      tmpl.querySelector("#imgBox").outerHTML = "";
+      tmpl.querySelector("#txtBox").className = "txtBox-textOnly";
+    } else {
+      tmpl.querySelector("#imgBox").innerHTML =
+        '<video style="width: 100%;" loop="loop" autoplay="autoplay" src=" ' +
+        temp.url +
+        ' "/>';
+      tmpl.querySelector("#txtBox").className = "txtBox";
+    }
 
-		tmpl.querySelector('#txtBox').value = temp.noteText;
-		tableRef.appendChild (tmpl);
-	}
-	addListeners();
+    tmpl.querySelector("#txtBox").value = temp.noteText;
+    tableRef.appendChild(tmpl);
+  }
+  addListeners();
 
-	//save order of divs
-	var orderIndex = tableRef.querySelectorAll('.draggable-element');
+  //save order of divs
+  var orderIndex = tableRef.querySelectorAll(".draggable-element");
 }
 
 function saveOrder() {
-	//save order of divs
-	var orderIndex = document.querySelectorAll('.draggable-element');
+  //save order of divs
+  var orderIndex = document.querySelectorAll(".draggable-element");
 }
 
 function clickybutton() {
-	var note = new NoteTemplate;
-	cards.push(note);
-	//note.noteText = "Step: " + (cards.length)
-	note.url = document.getElementById('urlInput').value;
-	reflow();
+  var note = new NoteTemplate();
+  cards.push(note);
+  //note.noteText = "Step: " + (cards.length)
+  note.url = document.getElementById("urlInput").value;
+  reflow();
 }
